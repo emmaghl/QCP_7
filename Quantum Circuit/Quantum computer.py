f580a628b745cd70dd985880d55971a0547c66fd
plank-of-string <79596924+plank-of-string@users.noreply.github.com>
2023-01-31 15:16:27 +0000

    Add files via upload

---
+++ Quantum Circuit/Quantum computer.py
@@ -0,0 +1,12 @@
+
+
+class Quantum_Computer:
+
+	__init__(self, Qubits):
+		self.Register_Size = Qubits
+		self.X = Gates.X()
+		self.Y = Gates.Y()
+		self.Z = Gates.Z()
+		self.H = Gates.Hadamard()
+		self.RN = Gates.Root_Not()
+		
