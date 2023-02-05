ca8d0fb0bbd38c2f4cb236a1eabf55f8f818e68d
maegannderson <122869854+maegannderson@users.noreply.github.com>
2023-02-05 12:42:14 +0000

    Update Quantum computer.py

--- Quantum Circuit/Quantum computer.py
+++ Quantum Circuit/Quantum computer.py
@@ -43,6 +43,35 @@ class Gates:
     def C_not(self):
         return np.matmul(np.array([[1,0,0,0],[0,1,0,0],[0,0,0,1],[0,0,1,0]]), self.inpt)
 
+### Really not sure how to put this in ###
+
+##Defining vector states##
+
+Zero = np.array([[1.0],[0.0]])  #This is for the |0> vector state
+
+One = np.array([[0.0],[1.0]])   #This is for the |1> vector state
+
+##Defining qubit as superposition of eigenstate, Zero and One vector states##
+
+a = 0.9 #These can be changed, just probabilities of finding in state, 
+
+b = (1 - a**2)**0.5 #Normalizing condition as a**2 + b**2 = 1, but might want to make a "Normalize" function because I think this is sloppy
+
+Qubit = a*Zero + b*One #I'm guessing this is a good way to make a superposition? But It should be complex and I haven't figured out how to do that yet 
+
+print ("Qubit = ", Qubit) #just checking 
+
+##Testing a Hadamard gate on different vector states, not sure how to apply it to the qubit? ##
+
+Hadamard = 1./(2**0.5) * np.array([[1, 1], [1, -1]])
+
+H_0 = np.dot(Hadamard, Zero)
+
+H_1 = np.dot(Hadamard, One)
+
+print ("H_0 =", H_0) #just checking 
+
+print ("H_1 =", H_1) #just checking 
 
 
 
