c02b15519b77059e76284f7b084248ef55eccea5
plank-of-string <milarviej@hotmail.com>
2023-02-07 12:57:25 +0000

    Tensor product

--- Quantum Circuit/Quantum computer.py
+++ Quantum Circuit/Quantum computer.py
@@ -15,8 +15,8 @@ class Quantum_Computer:
         return tensorprod #ouput is linear tensor product (NOTE: matrix form infromation lost)
 
     def Qubit(self):
-        self.Zero = np.array([[1, 0],[0, 0]]) #This is |0> vector state
-        self.One = np.array([[0,0], [0,1]]) #This is |1> vector state
+        self.Zero = np.array([1, 0]) #This is |0> vector state
+        self.One = np.array([0,1]) #This is |1> vector state
         self.a = (0+1j)/2**0.5 #the norm of a**2 plus norm of b**2 should = 1, this is the porbability of finding qbit in either state, 
         self.b = (0+1j)/2**0.5 # the norm of b**2 by contrast is prob of finding qbit in state b. If measured, it will be in either a or b. 
         return a*Zero + b*One
