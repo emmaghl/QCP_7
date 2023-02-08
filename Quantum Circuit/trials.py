89760ee664db60e7d99e186ddb9476491d962c00
emmaghelfi <s2090245@ed.ac.uk>
2023-02-08 11:01:30 +0000

    worked on normalisation in trials.py

--- Quantum Circuit/trials.py
+++ Quantum Circuit/trials.py
@@ -1,26 +1,27 @@
 import numpy as np
+import cmath
 n = 4
 
 j = 2 ** n
 # returns j coefficients which normalise to 1
-coeffs = []
+coeffs = (0+0*1j)*np.zeros(j)
 for i in range(j):
-    coeffs.append(np.random.random() + np.random.random()*1j)
+    coeffs[i] = (np.random.random() + np.random.random()*1j)
 
+print(coeffs)
 
 norm = 0
 for i in range(j):
     norm += np.absolute(coeffs[i]) ** 2
-print(norm)
 norm = norm**0.5
-print(norm)
 
-coeffs2 = []
+coeffs2 = (0+0*1j)*np.zeros(j)
 for i in range(j):
     theta = np.random.random()*np.pi*2
-    newcoeff = np.absolute(coeffs[i])/norm
-    coeffs2.append(newcoeff)
+    newcoeff = np.absolute(coeffs[i])*(np.cos(theta)+np.sin(theta)*1j)/(norm**2)
+    coeffs2[i] = newcoeff
 
+print(coeffs2)
 
 sum = 0
 for i in range(j):
@@ -28,3 +29,4 @@ for i in range(j):
 
 print(sum)
 
+#idea is to get a random angle and set the absolute value to 1/2**n. (would reduce comp. time)
