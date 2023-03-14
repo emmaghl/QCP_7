1df1efd34d38e0ae798813f8f39d563f775753e7
plank-of-string <milarviej@hotmail.com>
2023-03-14 13:13:49 +0000

    Added some comments

--- QuantumComputerSimulator/mods/SparseMatrix.py
+++ QuantumComputerSimulator/mods/SparseMatrix.py
@@ -6,6 +6,11 @@ import numpy as np
 class SparseMatrix(MatrixFrame):
 
     def __init__(self, Type: str, *args):
+        '''
+        Sets up gates initally in the dense method then condenses into sparse matrices.
+        :param Type: Take in the gate to be built
+        :param args:
+        '''
         if Type == 'H':  # hadamard gate
             self.matrix = 1 / math.sqrt(2) * np.array([0, 0, 1], [0, 1, 1], [1, 0, 1], [1, 1, -1])
         if Type == 'I':  # identity gate
@@ -36,7 +41,11 @@ class SparseMatrix(MatrixFrame):
         self.dim = self.size_matrix()[0]
 
     def size_matrix(self):
-        ncol = self.matrix[-1][0] + 1  # number of columns is the coloumn value of the last entry in the sparse matrix
+        '''
+        Gives the dimensions of the matrix. Used to preserve matrix structure information.
+        :return:
+        '''
+        ncol = self.matrix[-1][0] + 1  # number of columns is the column value of the last entry in the sparse matrix
         nr = 0  # number of rows is the maximum row value across the array (+1 because of Python indexing)
         for j in range(len(self.matrix)):
             if self.matrix[j][1] > nr:
@@ -47,6 +56,12 @@ class SparseMatrix(MatrixFrame):
 
     @classmethod
     def tensor_prod(self, m1, m2):
+        '''
+        Preform a tensor product between two matrices
+        :param m1: Matrix 1
+        :param m2: Matrix 2
+        :return: Tensor product of Matrix 1 with Matrix 2
+        '''
         m2_col = self.size_matrix(m2)[0]  # STcol/SM1col = SM2col etc.
         m2_row = self.size_matrix(m2)[1]
 
@@ -62,6 +77,12 @@ class SparseMatrix(MatrixFrame):
 
     @classmethod
     def matrix_multiply(cls, m1, m2):
+        '''
+        Multiply two matrices
+        :param m1: Matrix 1
+        :param m2: Matrix 2
+        :return: Matrix 1 multiplied by Matrix 2
+        '''
         # Convert SM1 and SM2 to a dictionaries with (row, col) keys and values for matrix manipulation when adding terms for matrix multiplication
         dict1 = {(row, col): val for row, col, val in m1}
         dict2 = {(row, c): v for row, c, v in m2}
@@ -77,6 +98,10 @@ class SparseMatrix(MatrixFrame):
         return matmul
 
     def transpose(M):
+        '''
+        Method to transpose a sparse matrix
+        :return: Matrix transposed
+        '''
         M_transpose = M.copy()
         print(M_transpose)
         for i in range(len(M)):
@@ -86,10 +111,20 @@ class SparseMatrix(MatrixFrame):
 
     @classmethod
     def inner_prod(cls, M):
+        '''
+        Inner product of matrix M
+        :param M: input matrix
+        :return: Transpose
+        '''
         return cls.matrix_multiply(M.matrix, cls.transpose(np.conj(M.matrix)))
 
     @classmethod
     def trace(cls, M):
+        '''
+        Trace of a sparse matrix.
+        :param M: Input Matrix
+        :return: Transpose of matrix
+        '''
         trace = 0
         for i in range(len(M)):
             for j in range(cls.size_matrix()[1]): #number of columns
@@ -98,6 +133,11 @@ class SparseMatrix(MatrixFrame):
         return trace
 
     def Basis(self, N): # need to check it's doing what i want it to
+        '''
+        Builds the sparse basis.
+        :param N:
+        :return: Returns the basis in the form of a list
+        '''
         Q = []
         for i in range(0, 2 ** N):
             Q.append([i,0,1])
@@ -106,6 +146,13 @@ class SparseMatrix(MatrixFrame):
         return Q
 
     def cnot(self, d, c, t):
+        '''
+        Inherits from MatrixFrame to produce a CNOT gate.
+        :param d:
+        :param c:
+        :param t:
+        :return:
+        '''
         digits = copy.deepcopy(d)
         cn = []
 
@@ -127,4 +174,9 @@ class SparseMatrix(MatrixFrame):
         pass
 
     def output(self, inputs):
+        '''
+        Output of sparse matrix class.
+        :param inputs:
+        :return:
+        '''
         return self.matrix_multiply(self.matrix, inputs)
